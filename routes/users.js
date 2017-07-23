9b1efb942da85327a0f25a2181f50b8509cc3052
Niko <nizh2008@gmail.com>
2017-07-23 22:18:22 +0800

    init project

---
+++ routes/users.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/normal', function(req, res, next) {
+	var result = {"result": "OK"};
+  res.send(JSON.stringify(result));
+});
+
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+
+
+module.exports = router;
